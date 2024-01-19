428c0ce802551e2e2f5b5a8e1bbd692de380e7b7
Brayan Garzon <506575@student.fontys.nl>
2024-01-19 13:25:35 +0100

    comments

--- menu.js
+++ menu.js
@@ -1,21 +1,29 @@
+// Select the menu button and the responsive menu
 let MenuButton = document.querySelector(".menu-btn");
 let Menu = document.querySelector(".responsive-menu");
 
-// determine if the menu is open or not
+// Determine if the menu is currently open or closed
 let IsMenuOpen = false;
 
-// if menu open, the icon position will change
-MenuButton.onclick = function(){
+// Event handler for the menu button click
+MenuButton.onclick = function() {
+    // Check if the menu is currently closed
     if (!IsMenuOpen) {
+        // Display the menu
         Menu.style.display = "block";
+        // Change the background position to show the open menu icon
         MenuButton.style.backgroundPosition = "center left 50px, center";
+        // Update the menu state to open
         IsMenuOpen = true;
     }
-
-    else if(IsMenuOpen) {
+    // Check if the menu is currently open
+    else if (IsMenuOpen) {
+        // Hide the menu
         Menu.style.display = "none";
+        // Change the background position to show the closed menu icon
         MenuButton.style.backgroundPosition = "center, center left 50px";
+        // Update the menu state to closed
         IsMenuOpen = false;
     }
-}
+};
 
